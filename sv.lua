00d787fa03e5d0d34cccce1d9bd92c42b59ce357
Parow <rya4an@gmail.com>
2019-02-26 19:43:03 +0100

    Re-ident + cleared

--- sv.lua
+++ sv.lua
@@ -1,31 +1,18 @@
-
-
-
-ESX = nil
-
+	ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-
 function CountJob()
 	local xPlayers = ESX.GetPlayers()
-
-
-	for k,v in pairs(JobList) do
-		--print(v)
-
+	for _,v in pairs(JobList) do
 		v.count = 0
-	for i=1, #xPlayers, 1 do
-		local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-		if xPlayer.job.name == v.job then
-			v.count = v.count + 1
+		for i=1, #xPlayers, 1 do
+			local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
+			if xPlayer.job.name == v.job then
+				v.count = v.count + 1
 
+			end
 		end
 	end
-	end
-
-	print(json.encode(JobList))
 	TriggerClientEvent("JobsConnected", -1, JobList)
-	
 	SetTimeout(5000, CountJob)
 end
 CountJob()
